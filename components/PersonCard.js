d59b4a82284d86feb177f32efc6a63afd9aec7be
Sia <siakaramalegos@gmail.com>
2019-05-24 12:56:54 -0500

    Replace organizers with real content

--- components/PersonCard.js
+++ components/PersonCard.js
@@ -1,12 +1,18 @@
-const PersonCard = ({name, image, bio, company, twitter, github}) => (
+import { GithubLink, TwitterLink, LinkedInLink, WebsiteLink, BlogLink } from "./SocialLink";
+
+const PersonCard = ({name, image, bio, company, twitter, github, linkedin, website, blog}) => (
   <div>
     <img src={image} alt={name} />
     <h1>{name}</h1>
     <h2>{company}</h2>
     <p>{bio}</p>
-    { twitter ? (<p>Twitter: {twitter}</p>) : null }
-    { github ? (<p>Github: {github}</p>) : null }
-    <p>Github: {github}</p>
+    <p>
+      {github ? <GithubLink username={github} /> : null}
+      {twitter ? <TwitterLink username={twitter} /> : null}
+      {linkedin ? <LinkedInLink url={linkedin} /> : null}
+      {website ? <WebsiteLink url={website} /> : null}
+      {blog ? <BlogLink url={blog} /> : null}
+    </p>
   </div>
 )
 
