d79e66a9492e156e78c941c3c1997da8825c6bef
Sia <siakaramalegos@gmail.com>
2019-06-18 17:02:40 -0500

    Add favicon

--- components/Layout.js
+++ components/Layout.js
@@ -30,6 +30,15 @@ const Layout = (props) => (
             font-family: 'Product Sans', "Helvetica", "Arial", sans-serif;}
         `}
       </style>
+      <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
+      <link rel="manifest" href="/static/site.webmanifest" />
+      <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#5bbad5" />
+      <link rel="shortcut icon" href="/static/favicon.ico" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="msapplication-config" content="/static/browserconfig.xml" />
+      <meta name="theme-col</meta>or" content="#ffffff" />
     </Head>
     <Navbar/>
     <Main>
